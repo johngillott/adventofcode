bb9fef25ee37339acb82da8cadc9c5385c4ed50a
John Gillott <8356592+johngillott@users.noreply.github.com>
2022-12-01 23:28:23 +0000

    day 2 wip

---
+++ rust/advent-of-code-2022/src/day2.rs
@@ -0,0 +1,161 @@
+#[aoc_generator(day2)]
+pub fn input_generator(input: &str) -> Vec<(char, char)> {
+    input
+        .lines()
+        .map(|x| {
+            let mut strategy: (char, char) = (' ', ' ');
+
+            match x.chars().nth(0).unwrap() {
+                'A' => strategy.0 = 'R',
+                'B' => strategy.0 = 'P',
+                'C' => strategy.0 = 'S',
+                _ => unreachable!(),
+            };
+
+            match x.chars().nth(2).unwrap() {
+                'X' => strategy.1 = 'R',
+                'Y' => strategy.1 = 'P',
+                'Z' => strategy.1 = 'S',
+                _ => unreachable!(),
+            };
+
+            strategy
+        })
+        .collect::<Vec<(char, char)>>()
+}
+
+use std::cmp::Ordering;
+
+fn cmp(p: &(char, char)) -> Ordering {
+    match (p.0, p.1) {
+        ('R', 'P') => Ordering::Greater,
+        ('R', 'R') => Ordering::Equal,
+        ('R', 'S') => Ordering::Less,
+        ('P', 'P') => Ordering::Equal,
+        ('P', 'R') => Ordering::Less,
+        ('P', 'S') => Ordering::Greater,
+        ('S', 'P') => Ordering::Less,
+        ('S', 'R') => Ordering::Greater,
+        ('S', 'S') => Ordering::Equal,
+        _ => unreachable!(),
+    }
+}
+
+#[aoc(day2, part1)]
+pub fn solve_part1(input: &[(char, char)]) -> u32 {
+    use std::cmp::Ordering;
+
+    input
+        .iter()
+        .map(|f| {
+            let mut score = 0;
+
+            match cmp(f) {
+                Ordering::Less => match f.1 {
+                    'R' => score = score + 1,
+                    'P' => score = score + 2,
+                    'S' => score = score + 3,
+                    _ => unimplemented!(),
+                },
+                Ordering::Equal => match f.1 {
+                    'R' => score = score + 1 + 3,
+                    'P' => score = score + 2 + 3,
+                    'S' => score = score + 3 + 3,
+                    _ => unimplemented!(),
+                },
+                Ordering::Greater => match f.1 {
+                    'R' => score = score + 1 + 6,
+                    'P' => score = score + 2 + 6,
+                    'S' => score = score + 3 + 6,
+                    _ => unimplemented!(),
+                },
+            }
+
+            score
+        })
+        .sum()
+}
+
+// #[aoc(day1, part2)]
+// pub fn solve_part2(input: &[(char, char)]) -> u32 {
+//     let mut sorted: Vec<u32> = input.iter().map(|elf| elf.iter().sum()).collect();
+
+//     sorted.sort_by(|a, b| b.cmp(a));
+
+//     sorted.iter().take(3).sum()
+// }
+
+#[cfg(test)]
+mod tests {
+
+    /*
+        A X Rock
+        B Y Paper
+        C Z Scissors
+
+    */
+    use super::*;
+
+    const INPUT1: &str = r#"A Y
+B X
+C Z"#;
+    #[test]
+    fn sample1() {
+        assert_eq!(solve_part1(&input_generator(INPUT1)), 15);
+    }
+
+    // your opponent will choose Rock (A), and you should choose Paper (Y).
+    // This ends in a win for you with a score of 8 (2 because you chose Paper + 6 because you won).
+    #[test]
+    fn sample2() {
+        assert_eq!(solve_part1(&input_generator(r#"A Y"#)), 8);
+    }
+
+    // In the second round, your opponent will choose Paper (B), and you should choose Rock (X).
+    // This ends in a loss for you with a score of 1 (1 + 0).
+    #[test]
+    fn sample3() {
+        assert_eq!(solve_part1(&input_generator(r#"B X"#)), 1);
+    }
+
+    // The third round is a draw with both players choosing Scissors, giving you a score of 3 + 3 = 6.
+    #[test]
+    fn sample4() {
+        assert_eq!(solve_part1(&input_generator(r#"C Z"#)), 6);
+    }
+
+    // Draw every match
+    const INPUT2: &str = r#"A X
+B Y
+C Z"#;
+    #[test]
+    fn sample5() {
+        assert_eq!(solve_part1(&input_generator(INPUT2)), 15);
+    }
+
+    // Win every match
+    const WIN_EVERY: &str = r#"A Y
+B Z
+C X"#;
+    #[test]
+    fn sample6() {
+        assert_eq!(solve_part1(&input_generator(WIN_EVERY)), 24);
+    }
+
+    // Lose every match
+    const LOSE_EVERY: &str = r#"A Z
+B X
+C Y"#;
+    #[test]
+    fn sample7() {
+        assert_eq!(solve_part1(&input_generator(LOSE_EVERY)), 6);
+    }
+
+    //     const INPUT2: &str = r#"C X
+    // B Y
+    // C Z"#;
+    //     #[test]
+    //     fn sample2() {
+    //         assert_eq!(solve_part1(&input_generator(INPUT2)), 12);
+    //     }
+}
