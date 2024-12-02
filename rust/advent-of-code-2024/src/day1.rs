e44c912dd4f3e081b3aa5b827709fbe43c5943bb
John Gillott <8356592+johngillott@users.noreply.github.com>
2024-12-02 09:37:41 +0000

    chore(2024): code tidy

--- rust/advent-of-code-2024/src/day1.rs
+++ rust/advent-of-code-2024/src/day1.rs
@@ -23,8 +23,6 @@ pub fn input_generator(input: &str) -> (LocationIds, LocationIds) {
 
 #[aoc(day1, part1)]
 pub fn solve_part1(input: &(LocationIds, LocationIds)) -> u32 {
-    // input.iter().map(|elf| elf.iter().sum()).max().unwrap()
-
     let iter = input.0.iter().zip(input.1.iter());
 
     iter.map(|(a, b)| a.abs_diff(*b)).sum()
